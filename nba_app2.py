d9a5f14bca39251727ab35b3019163a2dc4aa96e
apiqqq <afiqhilmy7@gmail.com>
2026-08-18 17:09:59 +0800

    Update nba_app2.py

--- nba_app2.py
+++ nba_app2.py
@@ -255,7 +255,7 @@ with top_left:
     # Watermark, Title, and Subtitle block
     st.markdown(
         f'''
-        <div class="header-watermark">ENGINEERED BY AFIQ HILMY | BASKETBALL ANALYTICS & PREDICTIVE MODELING</div>
+        <div class="header-watermark">BASKETBALL ANALYTICS AND MODELLING BY AFIQ HILMY | SPORTS ANALYTICS ENTHUSIAST</div>
         <div style="display: flex; align-items: center; margin-bottom: 6px;">
             {small_logo_tag}
             <h1 class="main-header">NBA EXPECTED POINTS PER GAME (XPPG) PREDICTION</h1>
