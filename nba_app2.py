e1c1a2a2beea25a8797f59cce7e7cbad89a08f53
apiqqq <afiqhilmy7@gmail.com>
2026-08-18 16:08:38 +0800

    Update fmt.Println to print 'Goodbye World'

--- nba_app2.py
+++ nba_app2.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.graph_objects as go
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
 from xgboost import XGBRegressor
