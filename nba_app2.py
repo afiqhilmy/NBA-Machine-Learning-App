e5e1ae7b18d614fee89e340b6bb35c9a49cbf717
apiqqq <afiqhilmy7@gmail.com>
2026-08-18 16:38:36 +0800

    Update nba_app2.py

--- nba_app2.py
+++ nba_app2.py
@@ -59,7 +59,7 @@ st.markdown("""
     }
 
     .header-logo {
-        height: 90px; /* Scaled logo to fill right side box */
+        height: 160px; /* Scaled logo to fill right side box */
         width: auto;
         object-fit: contain;
     }
